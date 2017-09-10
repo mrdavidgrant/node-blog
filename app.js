047e6866ceab1dc0a0a4d7e8b5b6245685a21168
David Grant <mrdavidgrant@outlook.com>
2017-09-10 20:35:12 +0000

    complete template from tutorial

---
+++ app.js
@@ -0,0 +1,59 @@
+/* app.js */
+
+//require and instantate express
+const app = require('express')()
+
+//fake posts to simulate a database
+const posts = [
+  {
+    id: 1,
+    author: 'John',
+    title: 'Templating with EJS',
+    body: 'First blog post'
+  },
+  {
+    id: 2,
+    author: 'Drake',
+    title: 'Express: Starting from the bottom',
+    body: 'Blog post number 2'
+  },
+  {
+    id: 3,
+    author: 'Emma',
+    title: 'Streams',
+    body: 'Blog post number 3'
+  },
+  {
+    id: 4,
+    author: 'Cody',
+    title: 'Events',
+    body: 'Blog post number 4'
+  }
+]
+
+//set the view engine to ejs
+app.set('view engine', 'ejs')
+
+//blog home page
+app.get('/', (req, res) => {
+  //render 'home.ejs' with the list of posts
+  res.render('home', { posts: posts })
+})
+
+//blog post
+app.get('/post/:id', (req, res) => {
+  const post = posts.filter((post) => {
+    return post.id == req.params.id
+  })[0]
+
+  //render the 'post.ejs' template with the post content
+  res.render('post', {
+    author: post.author,
+    title: post.title,
+    body: post.body
+  })
+})
+
+app.listen(8080)
+
+console.log('listenting port 8080')
